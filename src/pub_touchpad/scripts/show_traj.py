ffb9568eb1fef70547344a72fbc71af092575dce
WenzelHu <husiqixiang@gmail.com>
2020-02-18 04:11:12 +0100

    Add a dummy reset service and reduce sensitivity of the touchpad

--- src/pub_touchpad/scripts/show_traj.py
+++ src/pub_touchpad/scripts/show_traj.py
@@ -14,43 +14,67 @@ class displayer:
         self.currY = -1
         self.prevX = -1
         self.prevY = -1
+        self.h = []
         self.fig = plt.gcf()
         self.fig.show()
-        self.fig.canvas.draw()
-        self.h = []
         self.curr_plot_idx = -1
         plt.xlim([1200,5500])
         plt.ylim([-5000,-1200])
-
+        self.fig.canvas.draw()
+        self.mutex_h = Lock()
+        self.mutex_xy = Lock()
         self.subscriber = rospy.Subscriber("finger_position", Int32MultiArray, self.callback)
 
     def callback(self, msg):
-        #mutex.acquire()
-        self.currX = msg.data[0]
-        self.currY = -msg.data[1]
-        #mutex.release()
+        if msg.data[0] == 10000 and msg.data[1] == 10000:
+            rospy.loginfo("Reset the figure.")
+            self.mutex_h.acquire()
+            self.curr_plot_idx = -1
+            self.currX = -1
+            self.currY = -1
+            self.prevX = -1
+            self.prevY = -1
+            for plot_obj in self.h:
+                plot_obj[0].set_xdata([])
+                plot_obj[0].set_ydata([])
+            self.h = []
+            self.mutex_h.release()
+            rospy.loginfo("All cleared.")
+        else:
+            self.mutex_xy.acquire()
+            self.currX = msg.data[0]
+            self.currY = -msg.data[1]
+            self.mutex_xy.release()
     def draw(self):
         # plt.plot(self.currX, self.currY,'k.')
-        #mutex.acquire()
+        self.mutex_h.acquire()
+        self.mutex_xy.acquire()
         currX = self.currX
         currY = self.currY
+        self.mutex_xy.release()
         prevX = self.prevX
         prevY = self.prevY
-
         self.prevX = currX
         self.prevY = currY
-        #mutex.release()
         dist = np.sqrt((currX - prevX)**2 + (currY - prevY)**2)
-        if not self.h or dist > 400:
-            print("Distance:",dist)
+        if not self.h:
+            print("New figure. Creat new plot object array.")
             self.h.append(plt.plot([],[]))
             self.curr_plot_idx +=1
-        
-        self.h[self.curr_plot_idx][0].set_xdata(np.append(self.h[self.curr_plot_idx][0].get_xdata(), currX))
-        self.h[self.curr_plot_idx][0].set_ydata(np.append(self.h[self.curr_plot_idx][0].get_ydata(), currY))
-        self.h[self.curr_plot_idx][0].set_color('k')
-        self.h[self.curr_plot_idx][0].set_linewidth(5)
-        self.fig.canvas.draw()
+            self.fig.canvas.draw()
+        elif dist > 400:
+            print("New character. Append the plot object array.")
+            self.h.append(plt.plot([],[]))
+            self.curr_plot_idx +=1
+            self.fig.canvas.draw()
+
+        if dist != 0 and currX !=-1:
+            self.h[self.curr_plot_idx][0].set_xdata(np.append(self.h[self.curr_plot_idx][0].get_xdata(), currX))
+            self.h[self.curr_plot_idx][0].set_ydata(np.append(self.h[self.curr_plot_idx][0].get_ydata(), currY))
+            self.h[self.curr_plot_idx][0].set_color('k')
+            self.h[self.curr_plot_idx][0].set_linewidth(5)
+            self.fig.canvas.draw()
+        self.mutex_h.release()
 if __name__ == '__main__':
     rospy.init_node('traj_disp')
     displayer = displayer()
--- src/pub_touchpad/src/readTouchpad.cpp
+++ src/pub_touchpad/src/readTouchpad.cpp
@@ -20,14 +20,21 @@
 //namespace plt = matplotlibcpp;
 /* TODO: Close fd on SIGINT (Ctrl-C), if it's open */
 bool started = false;
-
+bool reset = false;
 
 bool startTrajCallback(std_srvs::Empty::Request& req,
                                 std_srvs::Empty::Response& resp)
 {
   ROS_INFO_STREAM("Start to collect touchpad");
-  started = true;
-  
+  started = true; 
+  return true;
+}
+
+bool resetCallback(std_srvs::Empty::Request& req,
+                                std_srvs::Empty::Response& resp)
+{
+  ROS_INFO_STREAM("Reset");
+  reset = true; 
   return true;
 }
 
@@ -35,7 +42,12 @@ bool startTrajCallback(std_srvs::Empty::Request& req,
 int main(int argc, char *argv[])
 {
     struct input_event ev;
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100000;
+    fd_set set;
     int fd = -77;
+    int result;
     char name[256] = "Unknown";
 
 
@@ -49,12 +61,16 @@ int main(int argc, char *argv[])
     ros::NodeHandle n;
     ros::Publisher finger_pub = n.advertise<std_msgs::Int32MultiArray>("finger_position", 1000);
     ros::ServiceServer srv_start = n.advertiseService("trajectory_start", startTrajCallback);
+    ros::ServiceServer srv_reset = n.advertiseService("trajectory_reset", resetCallback);
     std_msgs::Int32MultiArray msg;
 
     double currX = -1;
-    double currY = -1; 
+    double currY = -1;
     bool newX = false;
     bool newY = false;
+    bool largeDistX = false;
+    bool largeDistY = false;
+    int step = 0;
     ros::Rate r(50);
     
     while(ros::ok()) {
@@ -75,42 +91,97 @@ int main(int argc, char *argv[])
                 printf("device file = %s\n", EVENT_DEVICE);
                 printf("device name = %s\n", name);
             }
+            if(reset)
+            {
+                // ROS_INFO_STREAM("Publish reset message.");
+                msg.data.clear();
+                msg.data.push_back(10000);
+                msg.data.push_back(10000);
+                finger_pub.publish(msg);
+                reset = false;
 
-	        const size_t ev_size = sizeof(struct input_event);
-	        ssize_t size;
+            }
+
 
 	        /* TODO: use select() */
-
-	        size = read(fd, &ev, ev_size);
-	        if (size < ev_size) {
-	            fprintf(stderr, "Error size when reading\n");
-	            goto err;
-	        }
-
+            // ROS_INFO_STREAM("LOOP RUNNING1");
+	        // size = read(fd, &ev, ev_size);
+            FD_ZERO(&set);
+            FD_SET(fd, &set);
+            result = select(fd+1, &set, NULL, NULL, &tv);
+            if(result == -1)
+                ROS_INFO_STREAM("error.");
+            // else if (result == 0);
+            //     ROS_INFO_STREAM("No data received");
+            else if (result != 0)
+            {
+                const size_t ev_size = sizeof(struct input_event);
+                ssize_t size;
+                size = read(fd, &ev, ev_size);
+                if (size < ev_size) 
+                {
+                    fprintf(stderr, "Error size when reading\n");
+                    goto err;
+                }
+            }
 	        if (ev.type == EVENT_TYPE && (ev.code == EVENT_CODE_X
 	                      || ev.code == EVENT_CODE_Y)) {
 	            /* TODO: convert value to pixels */
 	            // printf("%s = %d\n", ev.code == EVENT_CODE_X ? "X" : "Y",
 	            //         ev.value);
 	            switch(ev.code){
-	            	case EVENT_CODE_X:  currX = ev.value;
-	            						newX = true;
+	            	case EVENT_CODE_X:  if(currX!=ev.value)
+                                        {
+                                            if(currX!=-1 && abs(currX - ev.value)>200)
+                                                largeDistX = true;
+
+                                            currX = ev.value;
+                                            newX = true;
+                                        }
+            
 	            					    break;
-	            	case EVENT_CODE_Y:  currY = ev.value;
-	            						newY = true;
+	            	case EVENT_CODE_Y:  
+                                        if(currY!=ev.value)
+                                        {
+                                            if(currY!=-1 && abs(currY - ev.value)>200)
+                                                largeDistY = true;
+                                            currY = ev.value;
+                                            newY = true;
+                                        }
 	            					    break;
 	            }
-	            if (newX && newY){
+	            if (newX && newY)
+                {
 	                // publish the point
-	                msg.data.clear();
-	            	msg.data.push_back(currX);
-	                msg.data.push_back(currY);
-	            	ROS_INFO("X = %d, Y = %d, send out this postion", msg.data[0], msg.data[1]);
-	   				finger_pub.publish(msg);
-	   				newX = false;
-	   				newY = false;         
+                    if(largeDistY||largeDistX)
+                    {
+                        if(step++ == 1)
+                        {
+                            msg.data.clear();
+                            msg.data.push_back(currX);
+                            msg.data.push_back(currY);
+                            finger_pub.publish(msg);
+                            newX = false;
+                            newY = false;
+                            largeDistX = false;
+                            largeDistY = false;
+                            step = 0;
+                        }
+                    }
+                    else if(!largeDistY&&!largeDistX)
+                    {
+                        msg.data.clear();
+                        msg.data.push_back(currX);
+                        msg.data.push_back(currY);
+                        finger_pub.publish(msg);
+                        newX = false;
+                        newY = false;   
+                    }
+                          
 	            } 
-	    	}    
+                    
+	    	}  
+            ros::spinOnce();  
             
         }
         ros::spinOnce();
